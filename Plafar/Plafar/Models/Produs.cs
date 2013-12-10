765e278b23317680fd22a231e954bc6328eecca8
GeorgianaSchipor <georgiana.schipor@gmail.com>
2013-12-10 20:54:58 +0200

    plafar - victor

---
+++ Plafar/Plafar/Models/Produs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Plafar.Models
+{
+    [Table("Produs")]
+    public class Produs
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Cod { get; set; }
+
+        public string Denumire { get; set; }
+        
+        public string Descriere { get; set; }
+    }
+}
